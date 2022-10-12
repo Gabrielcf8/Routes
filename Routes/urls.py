77e68e81a0c0a720a268b0e3f2b7012593ba1c21
Gabriel Carvalho de Faria <gabrielcf8@gmail.com>
2022-10-12 18:52:38 -0300

    Updated Django version

--- Routes/urls.py
+++ Routes/urls.py
@@ -1,25 +1,25 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from Routes.views import ListPoint, CreatePoint, CreateCategory, ListCategory, ListCity, CreateCity, CreateRoute, \
     ListRoute
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.firstview),
-    url(r'^cities', views.cityview),
-    url(r'^route', views.routeview),
-    url(r'^generate', views.generaterouteview),
-    url(r'^categories', views.categoryview),
-    url(r'^points', views.pointview, name='points'),
-    url(r'^map', views.mapview),
-    url(r'^user_profile/$', views.user_profile),
-    url(r'^user_form/$', views.create_profile),
-    url(r'^createpoint/$', CreatePoint.as_view(), name='cadastroPoint'),
-    url(r'^listpoint/$', ListPoint.as_view(), name='listaPoint'),
-    url(r'^createcategory/$', CreateCategory.as_view(), name='cadastroCategory'),
-    url(r'^listcategory/$', ListCategory.as_view(), name='listaCategory'),
-    url(r'^createcity/$', CreateCity.as_view(), name='cadastroCity'),
-    url(r'^listcity/$', ListCity.as_view(), name='listaCity'),
-    url(r'^createroute/$', CreateRoute.as_view(), name='cadastroRoute'),
-    url(r'^listroute/$', ListRoute.as_view(), name='listaRoute'),
+    re_path(r'^$', views.firstview),
+    re_path(r'^cities', views.cityview),
+    re_path(r'^route', views.routeview),
+    re_path(r'^generate', views.generaterouteview),
+    re_path(r'^categories', views.categoryview),
+    re_path(r'^points', views.pointview, name='points'),
+    re_path(r'^map', views.mapview),
+    re_path(r'^user_profile/$', views.user_profile),
+    re_path(r'^user_form/$', views.create_profile),
+    re_path(r'^createpoint/$', CreatePoint.as_view(), name='cadastroPoint'),
+    re_path(r'^listpoint/$', ListPoint.as_view(), name='listaPoint'),
+    re_path(r'^createcategory/$', CreateCategory.as_view(), name='cadastroCategory'),
+    re_path(r'^listcategory/$', ListCategory.as_view(), name='listaCategory'),
+    re_path(r'^createcity/$', CreateCity.as_view(), name='cadastroCity'),
+    re_path(r'^listcity/$', ListCity.as_view(), name='listaCity'),
+    re_path(r'^createroute/$', CreateRoute.as_view(), name='cadastroRoute'),
+    re_path(r'^listroute/$', ListRoute.as_view(), name='listaRoute'),
 ]
